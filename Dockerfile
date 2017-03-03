7ac2e0938ce3e02124c4869b135f1076d6805c6a
Don Humphreys <dhumphreys88@gmail.com>
2017-03-02 21:15:49 -0500

    Create parameterized dockerfile for building ruby images

--- Dockerfile
+++ Dockerfile
@@ -3,12 +3,16 @@ FROM penkit/alpine:latest
 # run as non-root user
 ENV RUN_AS ruby
 
-# run setup script to install ruby
-COPY ruby-setup.sh /usr/local/sbin/
-RUN ruby-setup.sh
-
 # default to ruby app directory
 WORKDIR /opt/ruby
 
 # execute interactive ruby terminal by default
 CMD ["irb"]
+
+# set ruby version from build arguments
+ARG RUBY_VERSION
+ENV RUBY_VERSION ${RUBY_VERSION}
+
+# run setup script to install ruby
+COPY ruby-setup.sh /usr/local/sbin/
+RUN ruby-setup.sh
--- ruby-setup.sh
+++ ruby-setup.sh
@@ -6,14 +6,20 @@ addgroup ruby
 adduser -S -G ruby ruby
 
 # configure rubygems to skip documentation
-echo -e "install: --no-document\nupdate: --no-document" >> /etc/gemrc \
+echo -e "install: --no-document --no-rdoc --no-ri" >> /etc/gemrc
+echo -e "update: --no-document --no-rdoc --no-ri" >> /etc/gemrc
 
-# install ruby, rubygems, bundler, and irb
+# install ruby, rubygems, and irb
 apk add --no-cache \
-  ruby \
-  ruby-bigdecimal \
-  ruby-bundler \
-  ruby-irb
+  ruby=$RUBY_VERSION \
+  ruby-bigdecimal=$RUBY_VERSION \
+  ruby-io-console=$RUBY_VERSION \
+  ruby-irb=$RUBY_VERSION \
+  ruby-json=$RUBY_VERSION \
+  ruby-rake=$RUBY_VERSION
+
+# install bundler gem
+gem install bundler
 
 # create a ruby app directory
 mkdir -p /opt/ruby
